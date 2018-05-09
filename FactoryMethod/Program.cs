c0abe682a25ea5d84b49fca623dae44397bc1d0c
Teodor Chirileanu <t.chirileanu@tricentis.com>
2018-05-09 18:55:19 +0200

    New DP: Factory Method

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FactoryMethod {
+    internal static class Program {
+        private static void Main() {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
